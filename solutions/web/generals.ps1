deb4a5332aa12ca464aa6623c741e47ea37c1afd
Guada <guada.casuso@gmail.com>
2013-07-26 06:59:34 -0400

    Create generals.ps1

---
+++ solutions/web/generals.ps1
@@ -0,0 +1,25 @@
+#Import-AzurePublishSettingsFile "Path"
+
+# help New-AzureWebsite
+#New-AzureWebsite "newwebsite"-Location "West US"
+
+# Get-Help New-AzureSqlDatabaseservers-detail
+#no help for SQLAzureDB
+
+## can you name the DB server??? 
+#New-AzureSqlDatabaseServer "pslkdblogin" -AdministratorLoginPassword "!Testpasswrord1" -Location "West US"
+
+#server name uc7zarv98d created.
+
+#create new DB
+#New-AzureSqlDatabase "azuredatabase" "x"
+
+#New DB Created
+
+
+#HOW DO YOU SET THE CONNECTION STRING- Whats the connectionstring property Bag format..
+# Get-Help New-AzureSqlDatabaseservers -Detailed
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+#New-AzureSqlDatabaseServer "server" -AdministratorLoginPassword "pass" -Location "West US"
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted 
