0850784f08db9d1a4f5cd88a3f3c758da4054338
rickrain <rickrain@hotmail.com>
2013-08-28 17:32:32 -0700

    Added changes to start scheduled tasks if running on batteries.

--- solutions/infrastructure/Start-AzureVMsOnSchedule.ps1
+++ solutions/infrastructure/Start-AzureVMsOnSchedule.ps1
@@ -12,28 +12,35 @@
         -TaskName "Start All Test Machines" -At 8:15AM
 #>
 
+
 param(
     # The name of the VM(s) to start on schedule.  Can be wildcard pattern.
     [Parameter(Mandatory = $true)] 
     [string]$VMName,
 
+
     # The service name that $VMName belongs to.
     [Parameter(Mandatory = $true)] 
     [string]$ServiceName,
+
 
     # The name of the scheduled task.
     [Parameter(Mandatory = $true)] 
     [string]$TaskName,
 
+
     # The name of the "Stop" scheduled tasks.
     [Parameter(Mandatory = $true)] 
     [DateTime]$At)
 
+
 # The script has been tested on Powershell 3.0
 Set-StrictMode -Version 3
 
+
 # Following modifies the Write-Verbose behavior to turn the messages on globally for this session
 $VerbosePreference = "Continue"
+
 
 # Check if Windows Azure Powershell is avaiable
 if ((Get-Module -ListAvailable Azure) -eq $null)
@@ -41,11 +48,16 @@ if ((Get-Module -ListAvailable Azure) -eq $null)
     throw "Windows Azure Powershell not found! Please install from http://www.windowsazure.com/en-us/downloads/#cmd-line-tools"
 }
 
+
 # Define a scheduled task to start the VM(s) on a schedule.
 $startAzureVM = "Start-AzureVM -Name " + $VMName + " -ServiceName " + $ServiceName + " -Verbose"
 $startTaskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Monday,Tuesday,Wednesday,Thursday,Friday -At $At
 $startTaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $startAzureVM
-$startScheduledTask = New-ScheduledTask -Action $startTaskAction -Trigger $startTaskTrigger
+$startTaskSettingsSet = New-ScheduledTaskSettingsSet  -AllowStartIfOnBatteries 
+
+$startScheduledTask = New-ScheduledTask -Action $startTaskAction -Trigger $startTaskTrigger -Settings $startTaskSettingsSet
+
 
 # Register the scheduled tasks to start and stop the VM(s).
 Register-ScheduledTask -TaskName $TaskName -InputObject $startScheduledTask
+
--- solutions/infrastructure/Stop-AzureVMsOnSchedule.ps1
+++ solutions/infrastructure/Stop-AzureVMsOnSchedule.ps1
@@ -9,29 +9,36 @@
     Stop-AzureVMsOnSchedule.ps1 -ServiceName "MyServiceName" -VMName "test*" -TaskName "Stop All Test Machines" -At 5:30PM
 #>
 
+
 param(
     # The name of the VM(s) to start on schedule.  Can be wildcard pattern.
     [Parameter(Mandatory = $true)] 
     [string]$VMName,
 
+
     # The service name that $VMName belongs to.
     [Parameter(Mandatory = $true)] 
     [string]$ServiceName,
 
+
     # The name of the scheduled task.
     [Parameter(Mandatory = $true)] 
     [string]$TaskName,
+
 
     # The name of the "Stop" scheduled tasks.
     [Parameter(Mandatory = $true)] 
     [DateTime]$At
 )
 
+
 # The script has been tested on Powershell 3.0
 Set-StrictMode -Version 3
 
+
 # Following modifies the Write-Verbose behavior to turn the messages on globally for this session
 $VerbosePreference = "Continue"
+
 
 # Check if Windows Azure Powershell is avaiable
 if ((Get-Module -ListAvailable Azure) -eq $null)
@@ -39,11 +46,16 @@ if ((Get-Module -ListAvailable Azure) -eq $null)
     throw "Windows Azure Powershell not found! Please install from http://www.windowsazure.com/en-us/downloads/#cmd-line-tools"
 }
 
+
 # Define a scheduled task to stop the VM(s) on a schedule.
-$stopAzureVM = "Stop-AzureVM -Name " + $VMName + " -ServiceName " + $ServiceName + " -StayProvisioned -Force -Verbose"
+$stopAzureVM = "Stop-AzureVM -Name " + $VMName + " -ServiceName " + $ServiceName + " -StayProvisioned -Force -Verbose > C:\WA-PowerShell\output.txt"
 $stopTaskTrigger = New-ScheduledTaskTrigger -Daily -At $At
 $stopTaskAction = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $stopAzureVM
-$stopScheduledTask = New-ScheduledTask -Action $stopTaskAction -Trigger $stopTaskTrigger
+$startTaskSettingsSet = New-ScheduledTaskSettingsSet  -AllowStartIfOnBatteries 
+
+$stopScheduledTask = New-ScheduledTask -Action $stopTaskAction -Trigger $stopTaskTrigger -Settings $startTaskSettingsSet
+
 
 # Register the scheduled tasks to start and stop the VM(s).
 Register-ScheduledTask -TaskName $TaskName -InputObject $stopScheduledTask
+
