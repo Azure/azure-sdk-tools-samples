677f6f6f44f61d5c124b514671bdf98aa290da3b
bindu <northtyphoon@gmail.com>
2013-11-20 21:29:32 -0800

    Update license

--- solutions/dev-test/VisualStudio2013/CreateNewADDSForest.ps1
+++ solutions/dev-test/VisualStudio2013/CreateNewADDSForest.ps1
@@ -1,6 +1,5 @@
 ﻿<#
     Copyright (c) Microsoft Corporation. All rights reserved.
-    This code is licensed as sample-code under the Visual Studio 2013 license terms.
     THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
     ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
     IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
